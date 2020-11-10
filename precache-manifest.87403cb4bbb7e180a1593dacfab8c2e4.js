e1dd4c0f10283370ee663305c3f760dab785503e
zaki <zachariasz.strus@gmail.com>
2020-11-10 15:45:45 +0100

    Updates

---
+++ precache-manifest.87403cb4bbb7e180a1593dacfab8c2e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb233c8da30e5fe9ce90e431193c11ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d82417ad9feacc4efe7",
+    "url": "/static/css/main.657e354a.chunk.css"
+  },
+  {
+    "revision": "7f49b3e654ee0404890f",
+    "url": "/static/js/2.a669f41a.chunk.js"
+  },
+  {
+    "revision": "0a91608fe6ead52bee082c2f509053e2",
+    "url": "/static/js/2.a669f41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d82417ad9feacc4efe7",
+    "url": "/static/js/main.b353fc86.chunk.js"
+  },
+  {
+    "revision": "0c190419de43bb3422de",
+    "url": "/static/js/runtime-main.41efb736.js"
+  }
+]);
